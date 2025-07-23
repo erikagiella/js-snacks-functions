b8dcaa47975a2e50c18c6fad3a524800e8a47f14
erikagiella <erikagiella93@gmail.com>
2025-07-23 19:23:42 +0200

    Es. 2 fatto

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+//Arrow Function
+const iniziali = array_stringhe => {
+    const iniziali = [];
+    for (let i = 0; i < names.length; i++){
+        iniziali.push(array_stringhe[i].charAt(0));
+    }
+    return iniziali;
+}
+
+
+//Function
+function iniziali_2 (array_stringhe) {
+    const iniziali = [];
+    for (let i = 0; i < names.length; i++){
+        iniziali.push(array_stringhe[i].charAt(0));
+    }
+    return iniziali;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log (iniziali(names));
+console.log (iniziali_2(names));
 
-
-
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"] 
