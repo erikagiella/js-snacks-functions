7e8d564fcda9f47c01adf162628a1abae9d0be62
erikagiella <erikagiella93@gmail.com>
2025-07-23 19:50:42 +0200

    Es. 5 fatto

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,12 +7,35 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
+const now = new Date();
+const ore = now.getHours();
 
 // Dichiara la funzione qui.
+//Arrow Function
+const salutoArrow = userName => {
+    if (ore <= 13){
+            return `Buongiorno ${userName}`
+    } else if (ore >= 13 && ore < 17){
+            return `Buon pomeriggio ${userName}`
+    }else{
+            return `Buonasera ${userName}`
+    }
+}
+
+//Function
+function saluto_2 (userName){
+    if (ore <= 13){
+        return `Buongiorno ${userName}`
+    } else if (ore >= 13 && ore < 17){
+        return `Buon pomeriggio ${userName}`
+    }else{
+        return `Buonasera ${userName}`
+    }
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(salutoArrow(name));
+console.log(saluto_2(name));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
