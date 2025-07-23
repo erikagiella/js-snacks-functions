f8e6e954c2418f21d07f0762291d4e8637320ebd
erikagiella <erikagiella93@gmail.com>
2025-07-23 13:13:34 +0200

    Es. 1 fatto

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,11 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+// Arrow function
 const saluto = userName => `Ciao ${userName}`
+
+//Function
+function saluto_2 (userName){
+    return `Ciao ${userName}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log (saluto)
+console.log (saluto(userName))
+
+console.log (saluto_2(userName))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
